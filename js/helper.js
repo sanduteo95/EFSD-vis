33554ed03b6e2314dcd31819b5d678289989dfce
Teodora Sandu <tas458@cs.bham.ac.uk>
2019-01-07 14:35:23 +0000

    move the global graph in the helper module

--- js/helper.js
+++ js/helper.js
@@ -1,7 +1,14 @@
 define(function () {
     // needed by nodes/mod, nodes/prov, nodes/dep
-
-    exports.isNumber = function (n) {
+    function isNumber (n) {
         return !isNaN(parseFloat(n)) && isFinite(n);
+    }
+
+    // needed by goi-machine, link and node
+    var graph = null;
+
+    return {
+        isNumber: isNumber,
+        graph: graph
     };
 });
--- js/link.js
+++ js/link.js
@@ -1,4 +1,4 @@
-define(function() {
+define(['helper'], function(Helper) {
 
 	class Link {
 
@@ -11,7 +11,7 @@ define(function() {
 
 			this.colour = null;
 			this.penWidth = null;
-			this.addToGraph(global.mainGraph); // cheating
+			this.addToGraph(Helper.graph); // cheating
 			this.addToNode(); // cheating
 		}
 
